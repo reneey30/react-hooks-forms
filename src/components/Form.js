95fde27e110b9af72bc221c8700d87b6879cb899
Irene Njeru <irenenjeru8@gmail.com>
2022-05-12 08:33:31 +0300

    task completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,16 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event){
+    setFirstName(event.target.value);
+  }
+  function handleLastNameChange(event){
+    setLastName(event.target.value);
+  }
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange}value={firstName} />
+      <input type="text" onChannge={handleLastNameChange}value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
